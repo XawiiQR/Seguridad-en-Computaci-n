2fc74c49976637b337a14a77c5fdf58df6809efc
Javier Quispe rojas <xawii@MacBook-Air-de-Javier.local>
2024-10-18 18:42:49 -0500

    me llega al webo girunb.

--- Laboratorio01/BellLaPadula.py
+++ Laboratorio01/BellLaPadula.py
@@ -10,33 +10,33 @@ class Documento:
 
 class BellLaPadula:
     def puede_leer(self, usuario, documento):
-        # Un usuario puede leer un documento si su nivel de seguridad es mayor o igual
+        
         if usuario.nivel_de_seguridad >= documento.clasificacion:
             return f"{usuario.nombre} puede leer {documento.nombre}"
         else:
             return f"{usuario.nombre} no puede leer {documento.nombre}"
 
     def puede_escribir(self, usuario, documento):
-        # Un usuario puede escribir en un documento si su nivel de seguridad es menor o igual
+        
         if usuario.nivel_de_seguridad <= documento.clasificacion:
             return f"{usuario.nombre} puede escribir en {documento.nombre}"
         else:
             return f"{usuario.nombre} no puede escribir en {documento.nombre}"
 
-# Crear lista de documentos
+
 documentos = [
     Documento("Documento Confidencial", 2),
     Documento("Top Secret", 3),
     Documento("Documento Público", 1)
 ]
 
-# Crear un usuario con nivel de seguridad 3
+
 usuarioblp1 = Usuario("UsuarioBellLaPadula1", 3)
 
-# Crear instancia de BellLaPadula
+
 blp = BellLaPadula()
 
-# Verificar acceso de lectura y escritura para cada documento
+
 for doc in documentos:
     print(blp.puede_leer(usuarioblp1, doc))
     print(blp.puede_escribir(usuarioblp1, doc))
